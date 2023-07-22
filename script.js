3445271f025005aefa9c221dadddf5c595ed1b17
Shraddhasings <80159597+Shraddhasings@users.noreply.github.com>
2023-07-23 02:07:50 +0530

    Added HTML, CSS and js

---
+++ script.js
@@ -0,0 +1,5 @@
+const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=49177c4bb134c3e9952b4a4f3b6eafcb&page=1';
+const IMG_PATH = 'https://image.tmdb/t/p/w1280';
+const SEARCHAPI = 'https://api.themoviedb.org/3/search/movie?&api_key=49177c4bb134c3e9952b4a4f3b6eafcb&query=';
+
+//5:21:44
